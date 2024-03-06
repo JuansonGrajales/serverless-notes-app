111efcb9e7d166847c0eefb696c91faa8036d3fe
Juan Grajales <juan.grajales001@gmail.com>
2024-03-06 15:45:06 -0800

    notes ui

---
+++ notes-ui/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Header from './components/header'
+import Notes from './components/notes'
+
+function App() {
+
+
+  return (
+    <div className='main'>
+      <Header/>
+      <Notes/>
+    </div>
+  )
+}
+
+export default App
---
+++ notes-ui/src/components/note.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Note = ({id, text, editHandler, deleteHandler}) => {
+    return (
+        <div className='note'>
+            <div className='note_body'>{text}</div>
+            <div className='note_footer'>
+                <button className='note_button' onClick={()=> deleteHandler(id)}>Delete</button>
+                <button className='note_button' onClick={() => editHandler(id, text)}>Edit</button>
+            </div>
+        </div>
+    );
+}
+
+export default Note
